2c7b57a1bb4f178648274b4d95b848be7534f424
Carolina Apaza <carolinaapaza@hotmail.com>
2023-05-23 20:06:58 -0300

    Se sube estado inicial del proyecto demoUseBootstrap

---
+++ demoUseBootstrap/src/main/java/ar/edu/unju/fi/pv/aplication/DemoUseBootstrapApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pv.aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUseBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUseBootstrapApplication.class, args);
+	}
+
+}
---
+++ demoUseBootstrap/src/test/java/ar/edu/unju/fi/pv/aplication/DemoUseBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.pv.aplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUseBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

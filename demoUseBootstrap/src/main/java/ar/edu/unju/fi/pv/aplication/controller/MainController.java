6ba38d66d65905d8036c216890314abc63aaee36
caroapaza <apazacarolina@gmail.com>
2023-05-24 00:08:54 -0300

    modificaciones para mostrar título en index

--- demoUseBootstrap/src/main/java/ar/edu/unju/fi/pv/aplication/controller/MainController.java
+++ demoUseBootstrap/src/main/java/ar/edu/unju/fi/pv/aplication/controller/MainController.java
@@ -1,6 +1,7 @@
 package ar.edu.unju.fi.pv.aplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,7 +9,8 @@ public class MainController {
 
 	
 	@GetMapping("/inicio")
-	public String getIndexPage() {
+	public String getIndexPage(Model model) {
+		model.addAttribute("titulo", "Bienvenidos a Programación Visual");
 		return "index";
 	}
 }
